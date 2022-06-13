9ecd76dac8f934130ec4f06b96775c9c06e39086
Francisco Monteiro <francisco.monteiro@ua.pt>
2022-06-12 17:22:01 -0700

    Added Server and Client Communication

--- visualizer/maps_test/index.js
+++ visualizer/maps_test/index.js
@@ -1,17 +1,47 @@
-// Initialize and add the map
+var map;
+var rsu1;
+var rsu2;
+
 function initMap() {
-    // The location of Uluru
-    const uluru = { lat: -25.344, lng: 131.031 };
-    // The map, centered at Uluru
-    const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 4,
-      center: uluru,
+    const aveiro = { lat: 40.632728, lng: -8.657024};
+    var mapProp = {
+        center: aveiro,
+        zoom: 16.5,
+    };
+    var rsu1Location = new google.maps.LatLng(40.631190, -8.6580);
+    var rsu2Location = new google.maps.LatLng(40.631180, -8.6590);
+    map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+    rsu1 = new google.maps.Marker({
+        position: rsu1Location,
+        map: map,
+        icon: {
+            url: 'parking-icon.png',
+            scaledSize: new google.maps.Size(100,100)
+        } 
     });
-    // The marker, positioned at Uluru
-    const marker = new google.maps.Marker({
-      position: uluru,
-      map: map,
+
+    rsu2 = new google.maps.Marker({
+        position: rsu2Location,
+        map: map,
+        icon: {
+            url: 'parking-icon.png',
+            scaledSize: new google.maps.Size(100,100)
+        } 
     });
-  }
-  
-  window.initMap = initMap;
+}
+
+const sio = io();
+
+sio.on('connect', () => {
+    console.log('connected');
+});
+
+sio.on('disconnect', () => {
+    console.log('disconnected');
+});
+
+sio.on('send_coords', (data, cb) => {
+    var newLocation = new google.maps.LatLng(data.coords[0], data.coords[1]);
+    rsu1.setPosition(newLocation);
+    cb(newLocation)
+});
---
+++ visualizer/server_app.py
@@ -0,0 +1,32 @@
+#to run: gunicorn -k eventlet -w 1 --reload server_app:app
+
+import socketio
+import threading
+
+sio = socketio.Server()
+app = socketio.WSGIApp(sio, static_files={
+    '/': 'maps_test/'
+})
+
+
+
+def send_coords_task(sid):
+    result = sio.call('send_coords', {'coords': [40.631140, -8.6585]}, to=sid)
+    print(result)
+    sio.sleep(1)
+    threading.Timer(send_coords_task(sid))
+
+@sio.event
+def connect(sid, environ):
+    print(sid, 'connected')
+    sio.sleep(3)
+    sio.start_background_task(send_coords_task, sid)
+
+@sio.event
+def disconnect(sid):
+    print(sid, 'disconnected')
+
+#@sio.event
+#def sum(sid, data):
+#    result = data['numbers'][0] + data['numbers'][1]
+#    return result
